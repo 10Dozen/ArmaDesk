bbaa4f78eda9d57a3cacab94994b6a24f8893a9a
10Dozen <steel.mjr@gmail.com>
2016-06-15 21:54:22 +0300

    Update wikiHelper.js

--- wikihelper/src/js/wikiHelper.js
+++ wikihelper/src/js/wikiHelper.js
@@ -16,32 +16,22 @@ function escapeQuotes(str) {
 	return ( str.replace(new RegExp('"','g'),'""') );
 };
 
-function getCode() {	
-	var debriefing;
-	var debriefingBlockStart = "class CfgDebriefing\n{";
-	var debriefingBlockEnd = "\n};";
-	
-	var debriefingClasses = "";
-	for (var i = 0; i < Endings.length; i++) {
-		var Ending = Endings[i];
-		
-		var text = '\n	class ' + Ending.name
-			+ '\n	{'
-			+ '\n		title = "' + escapeQuotes(Ending.title) + '";'
-			+ '\n		subtitle = "' + escapeQuotes(Ending.subtitle) + '";'
-			+ '\n		description = "' + escapeQuotes(Ending.description) + '";'
-			+ '\n	};';
-
-		debriefingClasses = debriefingClasses + text;		
+function showFormattedCode(code, type) {
+	if (type == "SQF") {
+		$( "#result-tab-header > b" ).html("SQF Code");
+	} else {
+		$( "#result-tab-header > b" ).html("Git Wiki Code");
 	};
+	$( "#result-tab" ).css( "top", "15%" );
+	$( "#result-tab-data" ).html( code );
+	
+}
 
-	debriefing = debriefingBlockStart + debriefingClasses + debriefingBlockEnd;
-	return debriefing;
-};
+
 
 function getCodeToDisplay() {
 	var code = getCode();
-	code = code.replace(/<br \/>/g, "\n&lt;br /&gt;");
+	code = code.replace(/<br \/>/g, "\n&lt;br /&gt;");cod
 	code = code.replace(/</g, "&lt;");
 	code = code.replace(/>/g, "&gt;");
 	code = code.replace(/(\r\n|\n|\r)/g,"<br />");
@@ -139,20 +129,43 @@ var Specification = function () {
 	this.getWikiCode = function () {};
 	this.getSqfCode = function () {
 		Spec.get();
+		
+		
+		var descriptionLines = Spec.description.split("\n");
+		var description = descriptionLines[1];
+		for (var i=1; i< descriptionLines.length; i++) {
+			description += "<br /> *      " +  descriptionLines[i];
+		};
+		
+		var examples = "";
+		var examplesLines = Spec.examples.split("\n");
+		for (var i=0; i< examplesLines.length; i++) {
+			examples += "<br /> *      " +  examplesLines[i];
+		};
+		
 		var inputText = "";
 		for (var i=0; i< Spec.inputs.length; i++) {
 			var inputData = Spec.inputs[i];
-			inputText += "<br /> * " + i + ": "+ inputData.type + " - " + inputData.desc;
+			
+			var inputDescs = inputData.desc.split("\n");
+			var inputDesc = inputDescs[0];
+			for (var j=1; j < inputDescs.length; j++) {
+				inputDesc += "<br /> *                " +  inputDescs[i];
+			}
+			
+			inputText += "<br /> * " + i + ": "+ inputData.type + " - " + inputDesc;
 		};
 		
 		var codeBlocks = [
 			"/*"
 			, " * " + Spec.expression
-			, " * " + Spec.description
+			, " * " + description
 			, " * "
 			, " * INPUT:" + inputText
 			, " * OUTPUT: " + Spec.output
-			, " * " + Spec.examples
+			, " * "
+			, " * EXAMPLES:" + examples
+			, " */"
 		];
 		
 		var output = "";
@@ -171,146 +184,10 @@ $( document ).ready(function() {
 	$( ".btn-clear" ).on("click", function () {
 			Spec.clear();
 	});
+	$( ".btn-get-wiki" ).on("click", function () {
+		showFormattedCode( Spec.getWikiCode(), "WIKI" );
+	});
+	$( ".btn-get-sqf" ).on("click", function () {
+		showFormattedCode( Spec.getSqfCode(), "SQF" );
+	});
 });
-
-/*
-
-var a = [];
-
-function getEndingById(id) {
-	var ending = {};
-	for (var i = 0; i < Endings.length; i++) {
-		if (Endings[i].id == id) { 
-			ending = Endings[i]; 
-			break;
-		}
-	};	
-	return ending;
-}
-
-function checkDuplicateNames(name) {
-	var result = true;
-	for (var i = 0; i < Endings.length; i++) {
-		if (Endings[i].name == name) {
-			result = false;
-			break;
-		};
-	};
-	
-	if (name == "") { result = false };
-	return result;
-};
-
-
-var Ending = function () {
-	this.id = EndingsMaxId;
-	this.name = "END" + EndingsMaxId;
-	this.title = "";
-	this.subtitle = "";
-	this.description = "";
-	this.$form = $(
-		"<ul class='ending-item' endingId='" + this.id + "'>"
-		+ "<div class='ending-remove' title='Delete ending'>✖</div>"
-		+ "<span><div class='ending-name' title='Ending class name (no spaces allowed)'>" + this.name + "</div></span>"
-		+ "<li><div class='dl-1'>Title</div><div class='dl-2'>"
-		+ "<input class='topicInput ending-title' placeholder='Mission Accomplished'></input></div></li>"
-		+ "<li><div class='dl-1'>Subtitle</div><div class='dl-2'>"
-		+ "<input class='topicInput  ending-subtitle' placeholder='All mission objectives completed'></input></div></li>"
-		+ "<li><div class='dl-1'>Description</div><div class='dl-3'>"
-		+ "<textarea class='topicData ending-desc' cols='" + textAreaSettings.cols 
-			+ "' rows='" + textAreaSettings.rows + "'></textarea>"
-		+ "</div></li><hr /></ul>"	
-	);
-	
-	
-	this.setEndingName = function (newName) {
-		this.name = newName.replace(/[-[\]{}()*+?.,\\^$|#\s!@%=&]/g, "")	
-		$( this.$form ).find('.ending-name').html( this.name );
-		$( this.$form ).find('.ending-name').css("display", "inline-block");
-	};
-	this.draw = function () {
-		$( "#endings-form" ).append( this.$form );
-	}
-	
-	this.initEvents = function () {
-		$(this.$form).find('.ending-remove').on('click', function () {			
-			var self = getEndingById( $ ($(this).parents()[0]).attr("endingId")  );
-			self.remove();
-		});
-		
-		$(this.$form).find('.ending-name').on('click', function () {			
-			var id = $( $(this).parents()[1] ).attr("endingId");
-			var name = $( this ).html();
-			$( this ).css( "display", "none" );
-			
-			var $inputName = $(this).parent().append(
-				"<div class='ending-name-input' endingId='" + id + "'>"
-				+ "<input value='" + name + "'/>"
-				+  "<span class='ending-name-btn ending-name-accept'>✓</span>"
-				+ "<span class='ending-name-btn ending-name-decline'>✗</span></div>"
-			);			
-			
-			$( $inputName ).find('.ending-name-btn').on('click', function () {
-				$( $( this ).parent() ).find( '.ending-name-accept' ).off();
-				$( $( this ).parent() ).find( '.ending-name-decline' ).off();
-				$( $( this ).parent() ).remove();
-			})
-			$( $inputName ).find('.ending-name-accept').on('click', function () {
-				var newName = $( this ).parent().find('input').val();				
-				var ending = getEndingById( $(this).parent().attr("endingId")  );
-				
-				if ( !checkDuplicateNames(newName) ) { newName = ending.name; }
-				ending.setEndingName(newName);			
-			});
-			$( $inputName ).find('.ending-name-decline').on('click', function () {
-				var ending = getEndingById( $(this).parent().attr("endingId")  );
-				ending.setEndingName(ending.name);
-			});			
-		});
-		
-		$(this.$form).find('.ending-title').on('blur', function () {			
-			var self = getEndingById( $ ($(this).parents()[2]).attr("endingId")  );
-			self.title = $( this ).val();
-		});
-		$(this.$form).find('.ending-subtitle').on('blur', function () {			
-			var self = getEndingById( $ ($(this).parents()[2]).attr("endingId")  );
-			self.subtitle = $( this ).val();
-		});
-		$(this.$form).find('.ending-desc').on('blur', function () {			
-			var self = getEndingById( $ ($(this).parents()[2]).attr("endingId")  );
-			self.description = $( this ).val();
-		});
-	}
-	
-	this.remove = function () {
-		Endings.splice( Endings.indexOf(this), 1 );			
-		
-		$( this.$form ).find('.ending-remove').off();
-		$( this.$form ).find('.ending-name').off();
-		$( this.$form ).find('.ending-name-btn').off();
-		$( this.$form ).find('.ending-title').off();
-		$( this.$form ).find('.ending-subtitle').off();
-		$( this.$form ).find('.ending-desc').off();
-		
-		$( this.$form ).remove();
-		
-		console.log("Ending Removed");
-	}
-	
-	this.init = function () {
-		this.draw();
-		this.initEvents();	
-		
-		EndingsMaxId++;
-		Endings.push(this);
-	}
-	
-	this.init();	
-}
-
-function addEnding() {
-	var ending = new Ending();
-	console.log('Ending added');
-}
-
-*/
